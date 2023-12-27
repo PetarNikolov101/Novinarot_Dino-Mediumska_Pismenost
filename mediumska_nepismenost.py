7796ec0347ac51ee234f29660f0dde34d944d004
PetarNikolov101 <petarnikolov101@gmail.com>
2023-12-27 06:53:21 +0100

    Update mediumska_nepismenost.py
    simple game loop

--- mediumska_nepismenost.py
+++ mediumska_nepismenost.py
@@ -1,3 +1,20 @@
 import pygame
 
-print("Hello world!")
+class MediumskaNepismenost:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1000, 500))
+        self.bg_color = (255,255,255)
+        self.running = True
+        pygame.display.set_caption("MIK")
+    
+    def game_loop(self):   
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+            
+game = MediumskaNepismenost()
+game.game_loop()
