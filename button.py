7a943c1b820cf0c521d7ae9626f013973f89db36
PetarNikolov101 <petarnikolov101@gmail.com>
2024-01-17 20:46:13 +0100

    play button added

---
+++ button.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Button:
+    def __init__(self, main):
+        self.screen = main.screen
+        self.screen_rect = self.screen.get_rect()
+        
+        self.width, self.height = 100, 40
+        self.button_color = (102, 255, 255)
+        self.text_color = (0, 0, 0)
+        self.font = pygame.font.Font(None, 30)
+        
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.x = 400
+        self.rect.y = 200
+        
+        self.msg_image = self.font.render("PLAY", True, self.text_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+        
+    def draw_button(self):
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
--- mediumska_nepismenost.py
+++ mediumska_nepismenost.py
@@ -1,12 +1,15 @@
 import pygame
+from button import Button
 
 class MediumskaNepismenost:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((1000, 500))
-        self.bg_color = (255,255,255)
+        self.bg_color = (0,0,0)
         self.running = True
         pygame.display.set_caption("MIK")
+        
+        self.button = Button(self)
     
     def game_loop(self):   
         while self.running:
@@ -14,7 +17,10 @@ class MediumskaNepismenost:
                 if event.type == pygame.QUIT:
                     self.running = False
             self.screen.fill(self.bg_color)
+            self.button.draw_button()
             pygame.display.flip()
+            
+            
             
 game = MediumskaNepismenost()
 game.game_loop()
