4e6d34da15c72efcfce806284d04aa72404d4829
PetarNikolov101 <petarnikolov101@gmail.com>
2024-01-29 13:29:41 +0100

    character and his movement added

--- main_character.py
+++ main_character.py
@@ -1,5 +1,32 @@
 import pygame
 
 class MainCharacter:
-    def __init__(self):
+    def __init__(self, main):
         pygame.init()
+        self.dino = pygame.image.load("./images/dino.png")
+        self.dino_run_right = pygame.image.load("./images/run_right.png")
+        self.dino_run_left = pygame.image.load("./images/run_left.png")
+        self.rect = self.dino.get_rect()
+        self.screen = main.screen
+        self.screen_rect = self.screen.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+        
+        self.moving_left = False
+        self.moving_right = False
+    
+    def update(self):
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 6
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 6
+              
+    def blitme(self):
+        if not self.moving_left and not self.moving_right or (self.moving_right and self.moving_left):
+            self.screen.blit(self.dino, self.rect)
+        if self.moving_right and not self.moving_left:
+            self.screen.blit(self.dino_run_right, self.rect)     
+        if self.moving_left and not self.moving_right:
+            self.screen.blit(self.dino_run_left, self.rect)
+    
+
+            
--- mediumska_nepismenost.py
+++ mediumska_nepismenost.py
@@ -1,17 +1,35 @@
 import pygame
 from button import Button
+from main_character import MainCharacter
 
 class MediumskaNepismenost:
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1080, 450))
         self.bg_color = (0,0,0)
         self.running = False
         pygame.display.set_caption("MIK")
         self.background = pygame.image.load('./images/2210_w026_n002_2557b_p1_2557.jpg')
         
+        self.dino = MainCharacter(self)
         self.button = Button(self)
     
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.dino.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.dino.moving_left = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.dino.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.dino.moving_left = False
+
     def game_loop(self):   
         while not self.running:
             for event in pygame.event.get():
@@ -27,10 +45,11 @@ class MediumskaNepismenost:
             pygame.display.flip()
              
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+            self.clock.tick(60)
+            self._check_events()
+            self.dino.update()
             self.screen.blit(self.background, (0, 0))
+            self.dino.blitme()
             pygame.display.flip()
 game = MediumskaNepismenost()
 game.game_loop()
